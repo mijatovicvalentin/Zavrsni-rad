438b724f26482436e642a3ae611642d443036430
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-09-05 10:19:52 +0200

    InfinityBeyondControllers(swagger)

--- InfinityBeyondControllers/InfinityBeyondControllers/Program.cs
+++ InfinityBeyondControllers/InfinityBeyondControllers/Program.cs
@@ -4,27 +4,22 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.   
+// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+// prilagodba za dokumentaciju, èitati https://medium.com/geekculture/customizing-swagger-in-asp-net-core-5-2c98d03cbe52
 
 builder.Services.AddSwaggerGen(sgo => { // sgo je instanca klase SwaggerGenOptions
     // èitati https://devintxcontent.blob.core.windows.net/showcontent/Speaker%20Presentations%20Fall%202017/Web%20API%20Best%20Practices.pdf
-    var o = new Microsoft.OpenApi.Models.OpenApiInfo()
-    {
-        Title = "InfinityBeyond API",
-        Version = "v1",
-       
-        
-    };
-    sgo.SwaggerDoc("v1", o);
+    
 
 
 });
 
 
+// dodavanje baze podataka
 builder.Services.AddDbContext<InfinityBeyondContext>(o =>
     o.UseSqlServer(
         builder.Configuration.
