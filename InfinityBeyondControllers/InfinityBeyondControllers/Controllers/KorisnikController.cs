071bc5f391fdf743f38d6457117c7687ebf5fb20
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-09-09 13:09:09 +0200

    InfinityBeyond(swagger(+faker)

--- InfinityBeyondControllers/InfinityBeyondControllers/Controllers/KorisnikController.cs
+++ InfinityBeyondControllers/InfinityBeyondControllers/Controllers/KorisnikController.cs
@@ -54,6 +54,7 @@ namespace InfinityBeyondControllers.Controllers
 
             try
             {
+
                 _context.Korisnik.Add(korisnik);
                 _context.SaveChanges();
                 return StatusCode(StatusCodes.Status201Created, korisnik);
@@ -118,6 +119,7 @@ namespace InfinityBeyondControllers.Controllers
                 {
                     return BadRequest();
                 }
+                //napisati provjeru moze li se obrisati
 
                 _context.Korisnik.Remove(korisnikBaza);
                 _context.SaveChanges();
@@ -142,6 +144,68 @@ namespace InfinityBeyondControllers.Controllers
                 return StatusCode(StatusCodes.Status503ServiceUnavailable,
                                   ex);
             }
+        }
+
+        [HttpGet]
+        [Route("UnosUBazu")]
+        public string UnosUBazu()   
+        {
+            Korisnik s;
+            for (int i = 0; i <= 1000; i++)
+            {
+                s = new()
+                {
+                    //Ime = "Valentin" + i,
+                    Ime = Faker.Name.First(),
+                    //Prezime = "Mijatovic" + i,
+                    Prezime = Faker.Name.Last(),
+                    //Email = "randomi@gmail.com" + i,
+                    Email = Faker.Internet.Email(),
+                    Oib = Faker.Identification.SocialSecurityNumber().Substring(0,10)
+
+                 
+                    
+
+                };
+                _context.Korisnik.Add(s);
+            }
+            _context.SaveChanges();
+
+            return "Okej";
+        }
+
+        //ruta koja na svakom entitetu koji ima parnu sifru jednom
+        //atributu mjenja vrijednost i  sprema u bazu.
+
+        [HttpGet]
+        [Route("Zad2")]
+        public string Zad2()
+        {
+            var korisnici = _context.Korisnik.ToList();
+
+            foreach(var k in korisnici)
+            {
+                if(k.id % 2 == 0)
+                {
+                    k.Ime += "Ime";
+                    _context.Korisnik.Update(k);
+                }
+            }
+            _context.SaveChanges();
+
+            return "Okej";
+        }
+        //Napisati rutu koja vraća sumu svih sifri na odabranom entinetu
+
+        [HttpGet]
+        [Route("Zad3")]
+        public string Zad3()
+        {
+            var korisnici = _context.Korisnik.ToList();
+          
+            
+
+            return "Okej";
         }
     }
 }
